8ebbf27513fbef53a2ff79959cbb9bffcb24a12b
Miracle179 <prsky.ivan@gmail.com>
2023-04-13 12:52:10 +0200

    encryptText function takes two string input parameters inputFile and outputFile representing the names of the input and output files respectively. It reads text from the input file, pads it with spaces if necessary to make each line 32 characters long, and then performs byte-level encryption on the text by calculating the position, ASCII character code, and parity bit for each character. The encrypted bytes are then written to the output file in binary format.

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -31,3 +31,49 @@ int calculateExpression(int a, int b, int c, int d) {
     result = sum;
     return result;
 }
+
+void encryptText(const std::string& inputFile, const std::string& outputFile) {
+    std::ifstream fin(inputFile);
+    std::ofstream fout(outputFile, std::ios::binary);
+
+    if (!fin.is_open() || !fout.is_open()) {
+        std::cout << "Failed to open files!" << std::endl;
+        return;
+    }
+
+    std::string line;
+    int lineNumber = 0;
+    while (std::getline(fin, line)) {
+        // Add spaces to lines less than 32 characters
+        if (line.length() < 32) {
+            line.append(32 - line.length(), ' ');
+        }
+
+        for (int i = 0; i < line.length(); i++) {
+            // Calculate the position of the character in the line (5 bits)
+            int position = i + 1;
+
+            // Calculate the ASCII character code (8 bits)
+            int ascii = static_cast<int>(line[i]);
+
+            // Calculate the parity bit (1 bit)
+            int parity = 0;
+            for (int j = 0; j < 8; j++) {
+                parity ^= (ascii >> j) & 1;
+            }
+
+            // Combine the data into two bytes
+            int byte1 = (lineNumber << 6) | (position << 1) | (ascii >> 7);
+            int byte2 = ((ascii & 0x7F) << 1) | parity;
+
+            // Write the two bytes to the output file
+            fout.write(reinterpret_cast<const char*>(&byte1), sizeof(byte1));
+            fout.write(reinterpret_cast<const char*>(&byte2), sizeof(byte2));
+        }
+
+        lineNumber++;
+    }
+
+    fin.close();
+    fout.close();
+}
